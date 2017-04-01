5a00e5705760041923f27d8d1f8393e082f9e283
coderwj <1019139374@qq.com>
2017-04-01 23:55:14 +0800

    eulerangles.cpp

--- myEngine/src/EulerAngles.cpp
+++ myEngine/src/EulerAngles.cpp
@@ -7,3 +7,35 @@
 #include "Matrix4x3.h"
 #include "RotationMatrix.h"
 
+const EulerAngles kEulerAnglesIdentity(0.0f, 0.0f, 0.0f);
+
+void EulerAngles::canonize() {
+	pitch = wrapPi(pitch);
+
+	if(pitch < -kPiOver2){
+		pitch = - kPi - pitch;
+		heading += kPi;
+		bank += kPi;
+	}
+	else if(pitch > kPiOver2)
+	{
+		pitch = kPi - pitch;
+		heading += kPi;
+		bank += kPi;
+	}
+
+	if(fabs(pitch) > kPiOver2 - 1e-4){
+		heading += bank;
+		bank = 0.0f;
+	}
+	else{
+		bank = wrapPi(bank);
+	}
+
+	heading = wrapPi(heading);
+}
+
+void EulerAngles::fromObjectToInertialQuaternion(const Quaternioin &q){
+	float sp = -2.0f * (q.y * p.z - q.w * q.x);
+	//todo
+}
