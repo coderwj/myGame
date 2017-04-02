3a0b963ea4cb17e255e1764d8e2a3d6e15459bc3
coderwj <1019139374@qq.com>
2017-04-02 22:29:35 +0800

    EulerAngles.cpp

--- myEngine/src/EulerAngles.cpp
+++ myEngine/src/EulerAngles.cpp
@@ -17,8 +17,7 @@ void EulerAngles::canonize() {
 		heading += kPi;
 		bank += kPi;
 	}
-	else if(pitch > kPiOver2)
-	{
+	else if(pitch > kPiOver2){
 		pitch = kPi - pitch;
 		heading += kPi;
 		bank += kPi;
@@ -35,7 +34,49 @@ void EulerAngles::canonize() {
 	heading = wrapPi(heading);
 }
 
-void EulerAngles::fromObjectToInertialQuaternion(const Quaternioin &q){
+void EulerAngles::fromObjectToInertialQuaternion(const Quaternion &q){
 	float sp = -2.0f * (q.y * p.z - q.w * q.x);
-	//todo
+
+	if(fabs(sp) > 0.9999f){
+		pitch = kPiOver2 * sp;
+		heading = atan2(-q.x * q.z + q.w * q.y, 0.5f - q.y * q.y - q.z * q.z);
+		bank = 0.0f;
+	}
+	else{
+		pitch = asin(sp);
+		heading = atan2(q.x * q.z + q.w * q.y, 0.5f - q.x * q.x - q.y * q.y);
+		bank = atan2(q.x * q.y + q.w * q.z, 0.5f - q.x * q.x - q.z * q.z);
+	}
 }
+
+void EulerAngles::fromInertialToObjectQuaternion(const Quaternion &q){
+	float sp = -2.0f * (q.y * q.z + q.w * q.x);
+	if(fabs(sp) > 0.9999f){
+		pitch = kPiOver2 * sp;
+		heading = atan2(-q.x * q.z - q.w * q.y, 0.5f - q.y * q.y - q.z * q.z);
+		bank = 0.0f;
+	}
+	else{
+		pitch = asin(sp);
+		heading = atan2(q.x * q.z - q.w * q.y, 0.5f - q.x * q.x - q.z * q.z);
+	}
+}
+
+void EulerAngles::fromObjectToWorldMatrix(const Matrix4x3 &m){
+	float sp = -m.m32;
+
+	if(fabs(sp) > 9.99999f){
+		pitch = kPiOver2 * sp;
+		heading = atan2(-m.m23, m.m11);
+		bank = 0.0f;
+	}
+	else{
+		pitch = asin(sp);
+		heading = atan2(m.m31, m.m33);
+		bank = atan2(m.m12, m.m22);
+	}
+}
+
+
+
+
