41129b364cb766f963bcf40aa1926b4a4c2ee92a
coderwj <1019139374@qq.com>
2017-04-07 23:29:40 +0800

    quaternion.cpp

--- myEngine/src/Quaternion.cpp
+++ myEngine/src/Quaternion.cpp
@@ -7,3 +7,39 @@
 #include "EulerAngles.h"
 
 const Quaternion kQuaternionIdentity = {1.0f, 0.0f, 0.0f, 0.0f};
+
+void Quaternion::SetToRotateAboutX(float theta){
+	float thetaOver2 = theta * 0.5f;
+	w = cos(thetaOver2);
+	x = sin(thetaOver2);
+	y = 0.0f;
+	z = 0.0f;
+}
+
+void Quaternion::SetToRotateAboutY(float theta){
+	float thetaOver2 = theta * 0.5f;
+	w = cos(thetaOver2);
+	x = 0.0f;
+	y = sin(thetaOver2);
+	z = 0.0f;
+}
+
+void Quaternion::SetToRotateAboutZ(float theta){
+	float thetaOver2 = theta * 0.5f;
+	w = cos(thetaOver2);
+	x = 0.0f;
+	y = 0.0f;
+	z = sin(thetaOver2);
+}
+
+void Quaternion::SetToRotateAboutAxis(const Vector3 &axis, float theta){
+	assert(fabs(vectorMod(axis) - 1.0f) < 0.01f);
+
+	float thetaOver2 = theta * 0.5f;
+	float sinThetaOver2 = sin(thetaOver2);
+	w = cos(thetaOver2);
+	x = axis.x * sinThetaOver2;
+	y = axis.y * sinThetaOver2;
+	z = axis.z * sinThetaOver2;
+}
+
