605aab567c16a3d6b161c3e2f80ef65f80a3a1a7
coderwj <1019139374@qq.com>
2017-04-18 01:33:35 +0800

    fix run error about gamescene init

--- src/gameSrc/gamescene.cpp
+++ src/gameSrc/gamescene.cpp
@@ -12,6 +12,7 @@
 
 #include <string>
 #include <vector>
+#include <fstream>
 
 
 #include "gamescene.h"
@@ -53,110 +54,120 @@ bool check_program_link_status(GLuint obj) {
     return true;
 }
 
+GLuint LoadShaders(GLFWwindow *window, const char * vertex_file_path, const char * fragment_file_path){
+
+    // Create the shaders
+    GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+    GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // Read the Vertex Shader code from the file
+    std::string VertexShaderCode;
+    std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
+    if(VertexShaderStream.is_open())
+    {
+        std::string Line = "";
+        while(getline(VertexShaderStream, Line))
+            VertexShaderCode += Line + "\n";
+        VertexShaderStream.close();
+    }
+
+    // Read the Fragment Shader code from the file
+    std::string FragmentShaderCode;
+    std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
+    if(FragmentShaderStream.is_open()){
+        std::string Line = "";
+        while(getline(FragmentShaderStream, Line))
+            FragmentShaderCode += Line + "\n";
+        FragmentShaderStream.close();
+    }
+
+    // Compile Vertex Shader
+    char const * VertexSourcePointer = VertexShaderCode.c_str();
+    glShaderSource(VertexShaderID, 1, &VertexSourcePointer , NULL);
+    glCompileShader(VertexShaderID);
+
+    // Compile Fragment Shader
+    char const * FragmentSourcePointer = FragmentShaderCode.c_str();
+    glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer , NULL);
+    glCompileShader(FragmentShaderID);
+
+    if(!check_shader_compile_status(VertexShaderID) || !check_shader_compile_status(FragmentShaderID)) {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return false;
+    }
+
+
+    // Link the program
+    GLuint ProgramID = glCreateProgram();
+    glAttachShader(ProgramID, VertexShaderID);
+    glAttachShader(ProgramID, FragmentShaderID);
+    glLinkProgram(ProgramID);
+
+    if(!check_program_link_status(ProgramID)) {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return false;
+    }
+
+    glDeleteShader(VertexShaderID);
+    glDeleteShader(FragmentShaderID);
+
+    return ProgramID;
+}
+
 bool GameScene::init(){
     int width = 640;
     int height = 480;
-    
-    
+
+
     if(glfwInit() == GL_FALSE) {
         std::cerr << "failed to init GLFW" << std::endl;
         return false;
     }
-    
+
     glfwSetErrorCallback(error_callback);
-    
+
     // select opengl version
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    
+
     // create a window
     GLFWwindow *window;
     if((window = glfwCreateWindow(width, height, "01shader_vbo1", 0, 0)) == 0) {
         std::cerr << "failed to open window" << std::endl;
         glfwTerminate();
-        return true;
+        return false;
     }
-    
+
     glfwMakeContextCurrent(window);
-    
-    // shader source code
-    std::string vertex_source =
-    "#version 330\n"
-    "layout(location = 0) in vec4 vposition;\n"
-    "layout(location = 1) in vec4 vcolor;\n"
-    "out vec4 fcolor;\n"
-    "void main() {\n"
-    "   fcolor = vcolor;\n"
-    "   gl_Position = vposition;\n"
-    "}\n";
-    
-    std::string fragment_source =
-    "#version 330\n"
-    "in vec4 fcolor;\n"
-    "layout(location = 0) out vec4 FragColor;\n"
-    "void main() {\n"
-    "   FragColor = fcolor;\n"
-    "}\n";
-    
+
+    if(glewInit() != GLEW_OK) {
+        std::cerr << "failed to init GLEW" << std::endl;
+        return false;
+    }
+
+    std::string vs_path = "/Users/coderwj/myGame/myEngine/res/shader/common.vs";
+    std::string ps_path = "/Users/coderwj/myGame/myEngine/res/shader/common.ps";
+
     // program and shader handles
-    GLuint shader_program, vertex_shader, fragment_shader;
-    
-    // we need these to properly pass the strings
-    const char *source;
-    int length;
-    
-    // create and compiler vertex shader
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    source = vertex_source.c_str();
-    length = vertex_source.size();
-    glShaderSource(vertex_shader, 1, &source, &length);
-    glCompileShader(vertex_shader);
-    if(!check_shader_compile_status(vertex_shader)) {
-        glfwDestroyWindow(window);
-        glfwTerminate();
-        return true;
-    }
-    
-    // create and compiler fragment shader
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    source = fragment_source.c_str();
-    length = fragment_source.size();
-    glShaderSource(fragment_shader, 1, &source, &length);
-    glCompileShader(fragment_shader);
-    if(!check_shader_compile_status(fragment_shader)) {
-        glfwDestroyWindow(window);
-        glfwTerminate();
-        return true;
-    }
-    
-    // create program
-    shader_program = glCreateProgram();
-    
-    // attach shaders
-    glAttachShader(shader_program, vertex_shader);
-    glAttachShader(shader_program, fragment_shader);
-    
-    // link the program and check for errors
-    glLinkProgram(shader_program);
-    if(!check_program_link_status(shader_program)) {
-        glfwDestroyWindow(window);
-        glfwTerminate();
-        return true;
-    }
-    
+    GLuint shader_program = LoadShaders(window, vs_path.c_str(), ps_path.c_str());
+
+
+
     // vao and vbo handle
     GLuint vao, vbo;
-    
+
     // generate and bind the vao
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
-    
+
     // generate and bind the buffer object
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    
+
     // data for a fullscreen quad
     GLfloat vertexData[] = {
         //  X     Y     Z           R     G     B
@@ -167,54 +178,50 @@ bool GameScene::init(){
         -1.0f, 1.0f, 0.0f,       0.0f, 1.0f, 0.0f, // vertex 4
         -1.0f,-1.0f, 0.0f,       1.0f, 0.0f, 0.0f, // vertex 5
     }; // 6 vertices with 6 components (floats) each
-    
+
     // fill with data
     glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*6*6, vertexData, GL_STATIC_DRAW);
-    
+
     // set up generic attrib pointers
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(GLfloat), (char*)0 + 0*sizeof(GLfloat));
-    
+
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(GLfloat), (char*)0 + 3*sizeof(GLfloat));
-    
+
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
-        
+
         // clear first
         glClear(GL_COLOR_BUFFER_BIT);
-        
+
         // use the shader program
         glUseProgram(shader_program);
-        
+
         // bind the vao
         glBindVertexArray(vao);
-        
+
         // draw
         glDrawArrays(GL_TRIANGLES, 0, 6);
-        
+
         // check for errors
         GLenum error = glGetError();
         if(error != GL_NO_ERROR) {
             std::cerr << error << std::endl;
             break;
         }
-        
+
         // finally swap buffers
         glfwSwapBuffers(window);
     }
-    
+
     // delete the created objects
-    
+
     glDeleteVertexArrays(1, &vao);
     glDeleteBuffers(1, &vbo);
-    
-    glDetachShader(shader_program, vertex_shader);
-    glDetachShader(shader_program, fragment_shader);
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
+
     glDeleteProgram(shader_program);
-    
+
     glfwDestroyWindow(window);
     glfwTerminate();
     return true;
