22a23c4da7cfab0662759a4bb62cc1b77606d169
coderwj <1019139374@qq.com>
2017-04-17 23:47:11 +0800

    change gamescene

--- src/gameSrc/gamescene.cpp
+++ src/gameSrc/gamescene.cpp
@@ -10,6 +10,9 @@
 #include "glfw3.h"
 #include <iostream>
 
+#include <string>
+#include <vector>
+
 
 #include "gamescene.h"
 
@@ -18,6 +21,36 @@ GameScene * GameScene::gs = 0;
 static void error_callback(int error, const char* description)
 {
     fputs(description, stderr);
+}
+
+// helper to check and display for shader compiler errors
+bool check_shader_compile_status(GLuint obj) {
+    GLint status;
+    glGetShaderiv(obj, GL_COMPILE_STATUS, &status);
+    if(status == GL_FALSE) {
+        GLint length;
+        glGetShaderiv(obj, GL_INFO_LOG_LENGTH, &length);
+        std::vector<char> log(length);
+        glGetShaderInfoLog(obj, length, &length, &log[0]);
+        std::cerr << &log[0];
+        return false;
+    }
+    return true;
+}
+
+// helper to check and display for shader linker error
+bool check_program_link_status(GLuint obj) {
+    GLint status;
+    glGetProgramiv(obj, GL_LINK_STATUS, &status);
+    if(status == GL_FALSE) {
+        GLint length;
+        glGetProgramiv(obj, GL_INFO_LOG_LENGTH, &length);
+        std::vector<char> log(length);
+        glGetProgramInfoLog(obj, length, &length, &log[0]);
+        std::cerr << &log[0];
+        return false;
+    }
+    return true;
 }
 
 bool GameScene::init(){
@@ -40,22 +73,125 @@ bool GameScene::init(){
     
     // create a window
     GLFWwindow *window;
-    if((window = glfwCreateWindow(width, height, "00skeleton", 0, 0)) == 0) {
+    if((window = glfwCreateWindow(width, height, "01shader_vbo1", 0, 0)) == 0) {
         std::cerr << "failed to open window" << std::endl;
         glfwTerminate();
-        return false;
+        return true;
     }
     
     glfwMakeContextCurrent(window);
     
-    // creation and initialization of stuff goes here
-    // ...
+    // shader source code
+    std::string vertex_source =
+    "#version 330\n"
+    "layout(location = 0) in vec4 vposition;\n"
+    "layout(location = 1) in vec4 vcolor;\n"
+    "out vec4 fcolor;\n"
+    "void main() {\n"
+    "   fcolor = vcolor;\n"
+    "   gl_Position = vposition;\n"
+    "}\n";
+    
+    std::string fragment_source =
+    "#version 330\n"
+    "in vec4 fcolor;\n"
+    "layout(location = 0) out vec4 FragColor;\n"
+    "void main() {\n"
+    "   FragColor = fcolor;\n"
+    "}\n";
+    
+    // program and shader handles
+    GLuint shader_program, vertex_shader, fragment_shader;
+    
+    // we need these to properly pass the strings
+    const char *source;
+    int length;
+    
+    // create and compiler vertex shader
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    source = vertex_source.c_str();
+    length = vertex_source.size();
+    glShaderSource(vertex_shader, 1, &source, &length);
+    glCompileShader(vertex_shader);
+    if(!check_shader_compile_status(vertex_shader)) {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return true;
+    }
+    
+    // create and compiler fragment shader
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    source = fragment_source.c_str();
+    length = fragment_source.size();
+    glShaderSource(fragment_shader, 1, &source, &length);
+    glCompileShader(fragment_shader);
+    if(!check_shader_compile_status(fragment_shader)) {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return true;
+    }
+    
+    // create program
+    shader_program = glCreateProgram();
+    
+    // attach shaders
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    
+    // link the program and check for errors
+    glLinkProgram(shader_program);
+    if(!check_program_link_status(shader_program)) {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return true;
+    }
+    
+    // vao and vbo handle
+    GLuint vao, vbo;
+    
+    // generate and bind the vao
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    
+    // generate and bind the buffer object
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    
+    // data for a fullscreen quad
+    GLfloat vertexData[] = {
+        //  X     Y     Z           R     G     B
+        1.0f, 1.0f, 0.0f,       1.0f, 0.0f, 0.0f, // vertex 0
+        -1.0f, 1.0f, 0.0f,       0.0f, 1.0f, 0.0f, // vertex 1
+        1.0f,-1.0f, 0.0f,       0.0f, 0.0f, 1.0f, // vertex 2
+        1.0f,-1.0f, 0.0f,       0.0f, 0.0f, 1.0f, // vertex 3
+        -1.0f, 1.0f, 0.0f,       0.0f, 1.0f, 0.0f, // vertex 4
+        -1.0f,-1.0f, 0.0f,       1.0f, 0.0f, 0.0f, // vertex 5
+    }; // 6 vertices with 6 components (floats) each
+    
+    // fill with data
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*6*6, vertexData, GL_STATIC_DRAW);
+    
+    // set up generic attrib pointers
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(GLfloat), (char*)0 + 0*sizeof(GLfloat));
+    
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(GLfloat), (char*)0 + 3*sizeof(GLfloat));
     
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
         
-        // drawing etc goes here
-        // ...
+        // clear first
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        // use the shader program
+        glUseProgram(shader_program);
+        
+        // bind the vao
+        glBindVertexArray(vao);
+        
+        // draw
+        glDrawArrays(GL_TRIANGLES, 0, 6);
         
         // check for errors
         GLenum error = glGetError();
@@ -68,9 +204,18 @@ bool GameScene::init(){
         glfwSwapBuffers(window);
     }
     
+    // delete the created objects
+    
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    
+    glDetachShader(shader_program, vertex_shader);
+    glDetachShader(shader_program, fragment_shader);
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+    glDeleteProgram(shader_program);
+    
     glfwDestroyWindow(window);
     glfwTerminate();
-    
     return true;
-
 }
