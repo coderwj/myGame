15210d259f7e24e10b2058ef8e33c1f776b7a4c6
wjwork <1019139374@qq.com>
2017-07-05 20:45:52 +0800

    add to model.cpp

--- myEngine/include/Model.h
+++ myEngine/include/Model.h
@@ -2,6 +2,10 @@
 #include "assimp/Importer.hpp"
 #include "assimp/scene.h"
 #include "assimp/postprocess.h"
+
+#include "Mesh.h"
+#include "Shader.h"
+
 using namespace std;
 
 class Model
@@ -28,5 +32,28 @@ private:
 	void loadModel(const string &path)
 	{
 		Assimp::Importer importer;
+		const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace)
+		if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+		{
+			cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << endl;
+			return;
+		}
+		m_directory = path.substr(0, path.find_last_of('/'));
+
+		processNode(scene->mRootNode, scene);
+	}
+
+	void processNode(aiNode * node, const aiScene * scene)
+	{
+		for(unsigned int i = 0; i < node->mNumMeshes; i++)
+		{
+			aiMesh * mesh = scene->mMeshes[node->mMeshes[i]];
+			m_meshs.push_back(processMesh(mesh, scene));
+		}
+	}
+
+	Mesh processMesh(aiMesh * mesh, const aiScene * scene)
+	{
+		
 	}
 }
