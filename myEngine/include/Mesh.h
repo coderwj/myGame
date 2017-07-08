7e080a6e1637c50a00c7a5d738e16a86830dfbe9
wjwork <1019139374@qq.com>
2017-07-08 18:00:27 +0800

    finish mesh.h, next step to load model by assimp.

--- myEngine/include/Mesh.h
+++ myEngine/include/Mesh.h
@@ -11,6 +11,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -44,13 +45,74 @@ public:
 		setupMesh();
 	}
 
+	void Draw(Shader shader)
+	{
+		unsigned int diffuseNr = 1;
+		unsigned int specularNr = 1;
+		unsigned int normalNr = 1;
+		unsigned int heightNr = 1;
+		for(unsigned int i = 0; i < textures.size(); i++)
+		{
+			glActiveTexture(GL_TEXTURE0 + i);
+
+			stringstream ss;
+			string number;
+			string name = textures[i].type;
+			if(name == "texture_diffuse")
+				ss << diffuseNr++;
+			else if(name == "texture_specular")
+				ss << specularNr++;
+			else if(name == "texture_normal")
+				ss << normalNr++;
+			else if(name == "texture_height")
+				ss << heightNr++;
+
+			number = ss.str();
+
+			glUniformli(glGetUniformLocation(shader.ID, (name + number).c_str()), i);
+			glBindTexture(GL_TEXTURE_2D, textures[i].id);
+
+		}
+
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		glActiveTexture(GL_TEXTURE0);
+	}
+
 private:
 
 	unsigned int VBO, EBO;
 
 	void setupMesh()
 	{
-		
+		glGenVertexArrays(1, &VAO);
+		glGenBufffers(1, &VBO);
+		glGenBufffers(1, &EBO);
+
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferDate(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+
+		glEnableVertexAttribArray(3);
+		glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Tangent));
+
+		glEnableVertexAttribArray(4);
+		glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Bitangent));
+
+		glBindVertexArray(0);
 	}
 
 };
