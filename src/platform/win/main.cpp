88f4f3baed1b06438334f61045463f410e686e96
coderwj <wj142417@gmail.com>
2017-07-25 23:41:22 +0800

    add imgui to myengine, to fix the render bug

--- src/platform/win/main.cpp
+++ src/platform/win/main.cpp
@@ -5,6 +5,9 @@
 #include "gtc/matrix_transform.hpp"
 #include "gtc/type_ptr.hpp"
 
+#include "imgui.h"
+#include "imgui_impl_glfw_gl3.h"
+
 #include <iostream>
 
 #include "Gamescene.h"
@@ -124,6 +127,12 @@ int main(){
     glfwSetScrollCallback(window, scroll_callback);
     glfwSetKeyCallback(window, key_callback);
 
+    ImGui_ImplGlfwGL3_Init(window, true);
+
+    bool show_test_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImColor(114, 144, 154);
+
     GameScene * pGameScene = GameScene::getInstance();
     if(pGameScene == NULL)
     {
@@ -139,6 +148,36 @@ int main(){
     while(!glfwWindowShouldClose(window))
     {
 
+        ImGui_ImplGlfwGL3_NewFrame();
+
+        // 1. Show a simple window
+        // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
+        {
+            static float f = 0.0f;
+            ImGui::Text("Hello, world!");
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+            ImGui::ColorEdit3("clear color", (float*)&clear_color);
+            if (ImGui::Button("Test Window")) show_test_window ^= 1;
+            if (ImGui::Button("Another Window")) show_another_window ^= 1;
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        }
+
+        // 2. Show another simple window, this time using an explicit Begin/End pair
+        if (show_another_window)
+        {
+            ImGui::SetNextWindowSize(ImVec2(200,100), ImGuiSetCond_FirstUseEver);
+            ImGui::Begin("Another Window", &show_another_window);
+            ImGui::Text("Hello");
+            ImGui::End();
+        }
+
+        // 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
+        if (show_test_window)
+        {
+            ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
+            ImGui::ShowTestWindow(&show_test_window);
+        }
+
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
@@ -150,7 +189,7 @@ int main(){
             break;
         }
         pGameScene->tick(deltaTime);
-
+        //ImGui::Render();
         glfwSwapBuffers(window);
         glfwPollEvents();
 
@@ -161,7 +200,7 @@ int main(){
             break;
         }
     }
-
+    ImGui_ImplGlfwGL3_Shutdown();
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
