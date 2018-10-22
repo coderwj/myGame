6d181e191bcf100505ef8459bddfc5efa28b9e62
coderwj <1019139374@qq.com>
2018-10-23 00:05:31 +0800

    animation.

---
+++ myEngine/src/Animation.h
@@ -0,0 +1,42 @@
+// #pragma once
+
+// namespace myEngine
+// {
+// 	class KeyFrame
+// 	{
+// 	public:
+// 		KeyFrame()
+// 		:x(0.f)
+// 		,y(0.f)
+// 		,z(0.f)
+// 		{
+
+// 		}
+
+// 		~KeyFrame()
+// 		{
+
+// 		}
+
+// 		float x;
+// 		float y;
+// 		float z;
+// 	};
+
+// 	class Animation
+// 	{
+// 	public:
+// 		Animation()
+// 		{
+
+// 		}
+// 		~Animation()
+// 		{
+
+// 		}
+
+// 	private:
+// 		std::vector<KeyFrame> m_keyFrames;
+// 	};
+
+// }
